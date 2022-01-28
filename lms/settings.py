6f0fbf74549e26d3a72553ddea8f2248f4b7b47f
Vivek Raj <raj.vivek999@gmail.com>
2022-01-29 00:42:00 +0530

    Secret key has been hidden using decouple

--- lms/settings.py
+++ lms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$qo3vf#irc7+^l9obhsn&k^1gsc1h!q#%h04fcep#raf5k%p=l'
+# SECRET_KEY = 'django-insecure-$qo3vf#irc7+^l9obhsn&k^1gsc1h!q#%h04fcep#raf5k%p=l'
+SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
